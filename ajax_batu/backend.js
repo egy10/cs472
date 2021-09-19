b4a855f591519623f6191f1a5ea225a0497b1adf
Egy <83844961+egy10@users.noreply.github.com>
2021-09-19 15:44:36 -0500

    USD -> MNT

---
+++ ajax_batu/backend.js
@@ -0,0 +1,13 @@
+const express = require('express')
+var bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.text());
+const port = 3000
+app.use(express.static('public'))
+app.post('/calc', (req, res) => {
+    console.log(req.body);
+    res.send("" + (parseInt(req.body) * 2854));
+  })
+
+app.listen(port, () => {
+})
