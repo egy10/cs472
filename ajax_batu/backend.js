08a8d114ee7157161df822c5dc321ddc889cd6bd
Egy <83844961+egy10@users.noreply.github.com>
2021-09-19 15:51:33 -0500

    UPDATE

--- ajax_batu/backend.js
+++ ajax_batu/backend.js
@@ -6,7 +6,7 @@ const port = 3000
 app.use(express.static('public'))
 app.post('/calc', (req, res) => {
     console.log(req.body);
-    res.send("" + (parseInt(req.body) * 2854));
+    res.send((parseInt(req.body) * 2854) + " MNT");
   })
 
 app.listen(port, () => {
